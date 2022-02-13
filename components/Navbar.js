ae5f4ae1c1229bf0db52a1b604a8d08a404e7843
Henry Chon 2 <chonhenry@outlook.com>
2022-02-12 21:48:24 -0500

    added logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import CodeIcon from "@mui/icons-material/Code";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
@@ -9,11 +10,14 @@ const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.nav_left}>
-        <div className={styles.nav_box}></div>
+        <div className={styles.nav_box}>
+          <CodeIcon />
+        </div>
         <div className={styles.nav_name_title}>
           <span className={styles.nav_name}>Henry Chon</span>
           <span className={styles.nav_title}>
-            <span className={styles.nav_spacer}>&nbsp;/</span> Software Engineer
+            <span className={styles.nav_spacer}>&nbsp;/</span>&nbsp;Software
+            Engineer
           </span>
         </div>
       </div>
