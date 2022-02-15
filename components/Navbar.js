b063ca0e9f63ae6608b7345b01cad5442ce734f4
Henry Chon 2 <chonhenry@outlook.com>
2022-02-15 00:15:03 -0500

    project page

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,23 +3,26 @@ import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import CodeIcon from "@mui/icons-material/Code";
 import styles from "../styles/Navbar.module.css";
+import Link from "next/link";
 
 const Navbar = ({ active, setActive }) => {
-  // const [active, setActive] = useState(false);
-
   return (
     <nav className={styles.navbar}>
       <div className={styles.nav_left}>
         <div className={styles.nav_box}>
           <CodeIcon />
         </div>
-        <div className={styles.nav_name_title}>
-          <span className={styles.nav_name}>Henry Chon</span>
-          <span className={styles.nav_title}>
-            <span className={styles.nav_spacer}>&nbsp;/&nbsp;</span>Software
-            Engineer
-          </span>
-        </div>
+        <Link href="/">
+          <a href="">
+            <div className={styles.nav_name_title}>
+              <span className={styles.nav_name}>Henry Chon</span>
+              <span className={styles.nav_title}>
+                <span className={styles.nav_spacer}>&nbsp;/&nbsp;</span>Software
+                Engineer
+              </span>
+            </div>
+          </a>
+        </Link>
       </div>
 
       <ul
@@ -27,9 +30,30 @@ const Navbar = ({ active, setActive }) => {
           active ? styles.nav_right + " " + styles.active : styles.nav_right
         }
       >
-        <li className={styles.nav_menu_item}>About</li>
-        <li className={styles.nav_menu_item}>Projects</li>
-        <li className={styles.nav_menu_item}>Contact</li>
+        <li
+          className={styles.nav_menu_item}
+          onClick={() => setActive((prev) => !prev)}
+        >
+          <Link href="/">
+            <a>About</a>
+          </Link>
+        </li>
+        <li
+          className={styles.nav_menu_item}
+          onClick={() => setActive((prev) => !prev)}
+        >
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
+        </li>
+        <li
+          className={styles.nav_menu_item}
+          onClick={() => setActive((prev) => !prev)}
+        >
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
       </ul>
 
       <div
